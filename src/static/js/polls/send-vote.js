1ad21ab4461d070f3ffac9a00702800659c12502
mykdolnyk <mdolnyk@gmail.com>
2025-08-13 11:04:51 +0300

    Enhanced the vote sending on the client's side - Included visual feedback when sending a vote

--- src/static/js/polls/send-vote.js
+++ src/static/js/polls/send-vote.js
@@ -1,26 +1,68 @@
-let votingForm = document.getElementById("voting-form")
-let submitButton = document.getElementById('voting-form-submit')
+const votingForm = document.getElementById("voting-form")
+const submitButton = document.getElementById('voting-form-submit')
+const pollResultsBlock = document.getElementById('poll-results')
+const choiceTemplate = document.getElementById('choice-template')
+submitButton.onclick = processSubmission
 
-submitButton.onclick = sendData
+let tm;
 
-var tm
+async function processSubmission() {
+    displaySubmission()
 
-import('https://cdn.jsdelivr.net/npm/@thumbmarkjs/thumbmarkjs/dist/thumbmark.umd.js').then(() => {
-    const fp = new ThumbmarkJS.Thumbmark();
-    fp.get().then((res) => {
-        tm = JSON.stringify(res)
-    })
-})
+    if (!tm) {
+        tm = await createThumb()
+    }
 
-function sendData() {
-    let formData = new FormData(votingForm)
+    const formData = new FormData(votingForm)
     formData.append('tm', tm)
-    fetch(submitVoteUrl, {
+    const response = await fetch(submitVoteUrl, {
         'method': 'POST',
         'body': formData
     })
-    .then((res) => {return res.json()})
-    .then(data => {
-        console.log(data)
-    })
+    if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+    }
+    const data = await response.json()
+    if (data.success) {
+        displayPollResults(data)
+    }
+    else {
+        displayVoteFail(data)
+    }
+}
+
+async function createThumb() {
+    await import('https://cdn.jsdelivr.net/npm/@thumbmarkjs/thumbmarkjs/dist/thumbmark.umd.js');
+    const tm = new ThumbmarkJS.Thumbmark();
+    const result = await tm.get()
+    return JSON.stringify(result)
+}
+
+function displaySubmission() {
+    votingForm.style.display = 'None'
+}
+
+function displayPollResults(data) {
+
+    for (choice of data.poll_data.choices) {
+        /** @type {DocumentFragment} */
+        let frag = choiceTemplate.content.cloneNode(true)
+
+        let newChoice = frag.querySelector('.choice-text')
+        newChoice.textContent = choice.text
+
+        let progressbar = frag.querySelector('.progressbar')
+        let progressbarBar = progressbar.firstElementChild
+
+        votePercent = (choice.total_votes / data.poll_data.total_votes) * 100
+        progressbarBar.style.width = `${votePercent}%`
+
+        pollResultsBlock.appendChild(frag)
+        }
+}   
+
+function displayVoteFail(data) {
+    votingForm.style.display = 'block'
+    votingForm.style.backgroundColor = 'red'
+    votingForm.appendChild('Error')
 }
