ec20ec5fdd884a2762662f1e9bbb3004b18424cd
mykdolnyk <mdolnyk@gmail.com>
2025-09-03 13:45:52 +0300

    Connected PostgreSQL to the project

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+echo "Upgrading the DB..."
+python -m flask --app run db upgrade
+
 exec "$@"
--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ import os
 DEBUG = (os.getenv('FLASK_DEBUG', 'False').lower() in ['yes', 'true', '1'])
 SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///dev.db"
+SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
 
 STATIC_FOLDER = 'static/'
 STATIC_URL_PATH = '/static/'
